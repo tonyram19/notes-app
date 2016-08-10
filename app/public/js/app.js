bd682aeeaf11cc458cf37653dc88cd544989bb03
Tony Ramirez <tonyram20@gmail.com>
2016-08-10 19:22:12 -0400

    Trying to make http requests work

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,27 +1,82 @@
 var app = angular.module("notesApp", []);
 
-app.directive("note", ['$http', function($http) {
+app.directive("notes", ['$http', function($http) {
     return {
         restrict: 'E',
-        templateUrl: '../views/note.html',
+        templateUrl: '../views/notes.html',
         controller: function($http) {
             thisCtrl = this;
 
             $http.get('/notes').then(function(response) {
                 thisCtrl.notes = response.data.notes;
             });
+
+
         },
-        controllerAs: 'NoteCtrl'
+        controllerAs: 'notes'
     };
 }]);
 
-app.directive("newNoteButton", [function() {
+app.directive("newNoteForm", ['$http', function($http) {
+    return {
+        restrict: 'E',
+        templateUrl: '../views/new-note-form.html',
+        controller: function($scope) {
+            this.submit = function() {
+                this.formClosed = true;
+
+                this.newNote = {
+                "note":
+                    {
+                        "title": $scope.noteTitle,
+                        "text": $scope.noteText,
+                        "date": new Date()
+                    }
+                };
+
+                this.closeForm = function() {
+                    this.formClosed = true;
+                };
+
+                var thisCtrl = this;
+                /*$http.post("/note", this.newNote).success(function(response) {
+                    thisCtrl.closeForm();
+                });
+*/
+                var request = new XMLHttpRequest();
+
+                request.onreadystatechange = function() {
+                  if (request.readyState == 4 && request.status == 200) {
+                    thisCtrl.closeForm();
+                    console.log(thisCtrl.formClosed);
+                  }
+                };
+request.open("POST", "/note", true);
+request.setRequestHeader("Content-type", "application/json");
+
+                request.send(JSON.stringify(this.newNote));
+
+            };
+        },
+        controllerAs: 'newNoteForm'
+    };
+}]);
+
+app.directive("newNoteButton", function() {
     return {
         restrict: 'E',
         templateUrl: '../views/new-note-button.html',
         controller: function() {
+            this.openForm = false;
 
+            this.openNewNoteForm = function() {
+                this.openForm = true;
+            };
+
+            this.closeNewNoteForm = function() {
+                this.openForm = false;
+            };
         },
-        controllerAs: 'NewNoteButtonCtrl'
+        controllerAs: 'newNoteButton'
     };
-}]);
+});
