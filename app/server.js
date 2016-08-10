c024a04ca2e6ac412411ac21df10c5ab41c78f25
Tony Ramirez <tonyram20@gmail.com>
2016-08-10 13:48:46 -0400

    Added Boostrap

--- app/server.js
+++ app/server.js
@@ -7,31 +7,31 @@ var app = express();
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 })
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html', { root: __dirname });
+    res.sendFile('index.html', { root: __dirname });
 });
 
 app.get('/notes', function (req, res) {
-  res.json({notes: db.findAll()});
+    res.json({notes: db.findAll()});
 });
 
 app.get('/notes/:id', function (req, res) {
-  var note = db.finById(req.params.id);
-  if(note) {
-    res.json({note: note});
-  } else {
-    res.sendStatus(404);
-  }
+    var note = db.finById(req.params.id);
+    if(note) {
+        res.json({note: note});
+    } else {
+        res.sendStatus(404);
+    }
 });
 
 app.post('/note', function (req, res) {
-  res.json({note: db.create(req.body.note)});
+    res.json({note: db.create(req.body.note)});
 });
 
 module.exports = app;
