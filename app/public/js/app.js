f07e6265ab672f87e124cf6abe30d0dcddf94993
Tony Ramirez <tonyram20@gmail.com>
2016-08-10 19:48:31 -0400

    Http requests working, can now get and post

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,82 +1,41 @@
 var app = angular.module("notesApp", []);
 
-app.directive("notes", ['$http', function($http) {
-    return {
-        restrict: 'E',
-        templateUrl: '../views/notes.html',
-        controller: function($http) {
-            thisCtrl = this;
+app.controller("MainController", ['$http', '$scope', function($http, $scope) {
 
-            $http.get('/notes').then(function(response) {
-                thisCtrl.notes = response.data.notes;
-            });
-
-
-        },
-        controllerAs: 'notes'
+    $scope.getNotes = function() {
+        $http.get('/notes').then(function(response) {
+            $scope.notes = response.data.notes;
+        });
     };
+
+    $scope.openNewNoteForm = function() {
+        $scope.openForm = true;
+    };
+
+    $scope.closeNewNoteForm = function() {
+        $scope.openForm = false;
+    };
+
+    $scope.submit = function() {
+
+        var newNote = {
+        "note":
+            {
+                "title": $scope.noteTitle,
+                "text": $scope.noteText,
+                "date": new Date()
+            }
+        };
+
+        $http.post("/note", newNote).then(function(response) {
+            $scope.getNotes();
+            $scope.closeNewNoteForm();
+            $scope.newNoteForm.$SetPristine();
+        });
+
+    };
+
+    $scope.openForm = false;
+    $scope.getNotes();
+
 }]);
-
-app.directive("newNoteForm", ['$http', function($http) {
-    return {
-        restrict: 'E',
-        templateUrl: '../views/new-note-form.html',
-        controller: function($scope) {
-            this.submit = function() {
-                this.formClosed = true;
-
-                this.newNote = {
-                "note":
-                    {
-                        "title": $scope.noteTitle,
-                        "text": $scope.noteText,
-                        "date": new Date()
-                    }
-                };
-
-                this.closeForm = function() {
-                    this.formClosed = true;
-                };
-
-                var thisCtrl = this;
-                /*$http.post("/note", this.newNote).success(function(response) {
-                    thisCtrl.closeForm();
-                });
-*/
-                var request = new XMLHttpRequest();
-
-                request.onreadystatechange = function() {
-                  if (request.readyState == 4 && request.status == 200) {
-                    thisCtrl.closeForm();
-                    console.log(thisCtrl.formClosed);
-                  }
-                };
-request.open("POST", "/note", true);
-request.setRequestHeader("Content-type", "application/json");
-
-                request.send(JSON.stringify(this.newNote));
-
-            };
-        },
-        controllerAs: 'newNoteForm'
-    };
-}]);
-
-app.directive("newNoteButton", function() {
-    return {
-        restrict: 'E',
-        templateUrl: '../views/new-note-button.html',
-        controller: function() {
-            this.openForm = false;
-
-            this.openNewNoteForm = function() {
-                this.openForm = true;
-            };
-
-            this.closeNewNoteForm = function() {
-                this.openForm = false;
-            };
-        },
-        controllerAs: 'newNoteButton'
-    };
-});
