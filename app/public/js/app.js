80708e98269745b36fde856bc3621f4fbae01253
Tony Ramirez <tonyram20@gmail.com>
2016-08-10 15:45:02 -0400

    Adding simple search functionality

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,10 +1,16 @@
 var app = angular.module("notesApp", []);
 
-app.controller("NotesController", function($http) {
+app.directive("note", ['$http', function($http) {
+    return {
+        restrict: 'E',
+        templateUrl: '../views/note.html',
+        controller: function($http) {
+            thisCtrl = this;
 
-    thisCtrl = this;
-
-    $http.get('/notes').then(function(response) {
-        thisCtrl.notes = response.data.notes;
-    });
-});
+            $http.get('/notes').then(function(response) {
+                thisCtrl.notes = response.data.notes;
+            });
+        },
+        controllerAs: 'NoteCtrl'
+    };
+}]);
